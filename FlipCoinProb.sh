507aa3c4d6cd6d421246c73ba1807f4757d1dc64
samiksha Shende <shende.samiksha@rediffmail.com>
2020-09-29 22:20:30 +0530

    Added : Flip Coin till head or tail reaches 21 code added

--- FlipCoinProb.sh
+++ FlipCoinProb.sh
@@ -30,3 +30,32 @@ fi
 done
 echo "Head won $H time"
 echo "Tail won $T time"
+
+
+# Flip the coin till Head or Tail have won 21 times
+
+H=0
+T=0
+while [ $H -ne 21 -a $T -ne 21 ]
+do
+R=$(( $RANDOM % 2 ))
+if [ $R -eq 0 ]
+then
+H=$(( $H + 1 ))
+else
+T=$(( $T + 1 ))
+fi
+done
+echo "Head $H"
+echo "Tail $T"
+if [ $H -eq 21 ]
+then
+D=$(( $H - $T ))
+echo "Head won by $D"
+elif [ $T -eq 21 ]
+then
+D=$(( $T - $H ))
+echo "Tail won by $D"
+else
+echo "It's Tie"
+fi
