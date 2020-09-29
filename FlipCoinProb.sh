9f08082f235be28073c144451f38dffc617c69b8
samiksha Shende <shende.samiksha@rediffmail.com>
2020-09-29 22:10:38 +0530

    Added : Display Winner code added

--- FlipCoinProb.sh
+++ FlipCoinProb.sh
@@ -1,3 +1,12 @@
 #!/bin/bash/
 echo "Welcome to the flip coin simulation problem." 
 
+# Flip Coin and display head or tail as a winner
+
+R=$(( $RANDOM % 2 ))
+if [ $R -eq 0 ]
+then
+echo "Head Won"
+else
+echo "Tail Won"
+fi
